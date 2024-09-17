d047a7b7642cc7a5464db1bc7cd91508212285da
zuhraildarovna <zuhraildarovna@icloud.com>
2024-09-17 22:25:09 +0500

    Add files via upload

---
+++ переменные(исправленный).py
@@ -0,0 +1,9 @@
+home_work=12
+print(home_work)
+numbers_of_hours_spent=1.5
+print(numbers_of_hours_spent)
+course_name=("python")
+print(course_name)
+time_for_one_task=numbers_of_hours_spent/home_work
+print(time_for_one_task)
+print("курс:",course_name,"всего задач:",home_work,"затрачено часов:",numbers_of_hours_spent,"среднее время выполнения:",time_for_one_task,"часа")
