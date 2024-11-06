c633ad2c351c37a973adb993797f162d6c660e88
zuhraildarovna <zuhraildarovna@icloud.com>
2024-11-06 22:42:43 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len('my_list'):
+    if my_list[a] < 0:
+        break
+
+    if my_list[a] > 0:
+        print(my_list[a])
+
+    a += 1
