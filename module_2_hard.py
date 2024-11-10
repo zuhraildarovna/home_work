6e79dc89c1904e8d349dd08fa32a8e319a7f3027
zuhraildarovna <zuhraildarovna@icloud.com>
2024-11-10 12:18:02 +0500

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,14 @@
+def get_password(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+n = int(input('Введите целое число от 3 до 20: '))
+
+result = get_password(n)
+print('Пароль:', result)
