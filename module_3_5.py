68a46fc2142ef3a6eaae033a48477325927a548b
zuhraildarovna <zuhraildarovna@icloud.com>
2024-11-12 22:19:10 +0500

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,11 @@
-def get_multiplied_digits(number):
+def get_multiplied_digits(number=0):
     str_number = str(number)
-    if len(str_number) > 1:
-        first = int(str_number[0])
+    first = int(str_number[0])
+    if len(str_number) >1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return int(str_number)
+    if first != 0:
+        return first
+    return 1
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(402030)
 print(result)
